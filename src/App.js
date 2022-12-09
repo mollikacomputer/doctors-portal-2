4cefefe3cf2a516cbd551c43885b58118fe58536
mollikacomputer <mollikacomputer3@gmail.com>
2022-12-09 20:52:39 +0600

    browser router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Navbar from './Pages/Shared/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <h2 className='text-3xl' > Doctors portal 2</h2>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element= {<Home/>} />
+        <Route path='/about' element= {<About/>} />
+        <Route path='/login' element= {<Login/>} />
+        <Route path='/register' element= {<Register/>} />
+      </Routes>
+  
     </div>
   );
 }
