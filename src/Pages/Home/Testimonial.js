beff65a697548900ed2defe282c380e75f21222d
mollikacomputer <mollikacomputer3@gmail.com>
2022-12-12 22:51:58 +0600

    testmonial section complete

--- src/Pages/Home/Testimonial.js
+++ src/Pages/Home/Testimonial.js
@@ -1,15 +1,24 @@
 import React from "react";
-import people1 from '../../assets/images/people1.png';
-const Testimonial = () => {
+
+const Testimonial = ({ review }) => {
   return (
     <section>
-      <div class="card lg:card-side bg-base-100 shadow-xl">
-        <figure>
-          <img src={people1} alt="Album" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <div className="card-body">
+          <div>
+            <p> {review.review}</p>
+          </div>
+          <div className="flex items-center">
+            <div className="avatar">
+              <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                <img src={review.photo} />
+              </div>
+            </div>
+            <div>
+              <h2 className="card-title">{review.name}</h2>
+              <p>{review.location}</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,6 +1,33 @@
 import React from "react";
 import QuoteImg from '../../assets/icons/quote.svg';
+import people1 from '../../assets/images/people1.png';
+import people2 from '../../assets/images/people2.png';
+import people3 from '../../assets/images/people3.png';
+import Testimonial from "./Testimonial";
 const Testimonials = () => {
+    const reviews = [
+      {
+        _id:1,
+        name:'William',
+        photo:people1,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, possimus.',
+        location:'New York'
+      },
+      {
+        _id:2,
+        name:'Mary',
+        photo:people2,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, possimus.',
+        location:'New York'
+      },
+      {
+        _id:3,
+        name:'May',
+        photo:people3,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, possimus.',
+        location:'New York'
+      }
+    ]
   return (
     <div className="my-32">
       <div className="flex justify-between">
@@ -12,8 +39,14 @@ const Testimonials = () => {
         <img className="w-12 lg:w-24" src={QuoteImg} alt="Album" />
         </div>
       </div>
-      <div>
-
+      <div className="grid grid-cols-1, md:grid-cols-2, lg:grid-cols-3 gap-5 mt-16">
+       {
+         reviews.map(review => <Testimonial 
+          key={review._id}
+          review = {review}
+          >
+            </Testimonial>)
+       }
 
       </div>
     </div>
