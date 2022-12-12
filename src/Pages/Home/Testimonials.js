8f4c58a7e7790d17f7263d81083152bc002a06e3
mollikacomputer <mollikacomputer3@gmail.com>
2022-12-12 22:00:24 +0600

    space between added

--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -2,14 +2,14 @@ import React from "react";
 import QuoteImg from '../../assets/icons/quote.svg';
 const Testimonials = () => {
   return (
-    <div>
-      <div>
-        <div className="flex flex-between">
+    <div className="my-32">
+      <div className="flex justify-between">
+        <div className="">
           <h3 className="text-primary text-start">Testimonials</h3>
           <h2 className="text-3xl text-start">What Our Patients Says</h2>
         </div>
         <div>
-        <img src={QuoteImg} alt="Album" />
+        <img className="w-12 lg:w-24" src={QuoteImg} alt="Album" />
         </div>
       </div>
       <div>
