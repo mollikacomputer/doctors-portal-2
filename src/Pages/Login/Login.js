f58170936526c0605a455d9a0573c163bfdfa20e
mollikacomputer <mollikacomputer3@gmail.com>
2023-01-02 23:23:03 +0600

    react hooks form added one field

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,22 +1,68 @@
 import React from "react";
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
+
 const Login = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-  if(user){
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  if (user) {
     console.log(user);
-  };
+  }
+  const onSubmit = (data) => console.log(data);
   return (
-    <div className="flex h-screen justify-center items-center " >
+    <div className="flex h-screen justify-center items-center ">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
-          <h2 className="text-center text-2xl font-bold">Card title!</h2>
+          <h2 className="text-center text-2xl font-bold"> Login </h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">What is your name?</span>
+                
+              </label>
+              <input
+                type="email"
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+                {...register("email", 
+                {
+                  required:{
+                    value:true,
+                    message:'email field is required'
+                  },
+                  pattern: {
+                    value: /[A-Za-z]{3}/,
+                    message: 'Please provide a valid message' 
+                  }
+                }
+                )}
 
-          <div className="divider" > Or </div>
-          <button 
-          class="btn btn-outline"
-          onClick={() => signInWithGoogle()}
-          >Continue With Google</button>
+              />
+              <label class="label">
+              {errors.email?.type === "required" && (<p role="alert" className="text-red-500"> {errors.email?.message} </p>)}
+              {errors.email?.type === "pattern" && (<p role="alert" className="text-red-500"> {errors.email?.message} </p>)} 
+              </label>
+            </div>
+
+           
+
+            <input
+              
+            />
+            {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+
+            <input type="submit" />
+          </form>
+
+          <div className="divider"> Or </div>
+          <button class="btn btn-outline" onClick={() => signInWithGoogle()}>
+            Continue With Google
+          </button>
         </div>
       </div>
     </div>
