880fb9137b28ae6f280a04b3c4e45a074c2ef78b
mollikacomputer <mollikacomputer3@gmail.com>
2023-01-06 14:27:23 +0600

    conditional log out added

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Navbar = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -56,7 +59,7 @@ const Navbar = () => {
             <Link to={"/about"}> About </Link>
           </li>
           <li>
-            <Link to={"/login"}>Login</Link>
+            {user ? <Link to={"/login"}>Log Out</Link> :<Link to={"/login"}>Login</Link>}
           </li>
           <li>
             <Link to={"/register"}>Register</Link>
